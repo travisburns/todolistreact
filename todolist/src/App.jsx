cfdb2c9b95e977d905cc12cc85b2feafaf297d58
Travis <travisjb0@gmail.com>
2023-07-20 17:21:27 -0700

    Added and recreated the first part of the tutorial: creating a add task from by myself.

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,75 +1,94 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
-// custom components
+// // custom components
+// import CustomForm from './components/CustomForm'
+// import EditForm from './components/EditForm'
+// import TaskList from './components/TaskList'
+
+// function App() {
+//   const [tasks, setTasks] = useState([]);
+//   const [previousFocusEl, setPreviousFocusEl] = useState(null);
+//   const [editedTask, setEditedTask] = useState(null);
+//   const [isEditing, setIsEditing] = useState(false);
+
+//   const addTask = (task) => {
+//     setTasks(prevState => [...prevState, task])
+//   }
+
+//   const deleteTask = (id) => {
+//     setTasks(prevState => prevState.filter(t => t.id !== id));
+//   }
+
+//   const toggleTask = (id) => {
+//     setTasks(prevState => prevState.map(t => (
+//       t.id === id
+//         ? { ...t, checked: !t.checked }
+//         : t
+//     )))
+//   }
+
+//   const updateTask = (task) => {
+//     setTasks(prevState => prevState.map(t => (
+//       t.id === task.id
+//         ? { ...t, name: task.name }
+//         : t
+//     )))
+//     closeEditMode();
+//   }
+
+//   const closeEditMode = () => {
+//     setIsEditing(false);
+//     previousFocusEl.focus();
+//   }
+
+//   const enterEditMode = (task) => {
+//     setEditedTask(task);
+//     setIsEditing(true);
+//     setPreviousFocusEl(document.activeElement);
+//   }
+
+//   return (
+//     <div className="container">
+//       <header>
+//         <h1>My Task List</h1>
+//       </header>
+//       {
+//         isEditing && (
+//           <EditForm
+//             editedTask={editedTask}
+//             updateTask={updateTask}
+//             closeEditMode={closeEditMode}
+//           />
+//         )
+//       }
+//       <CustomForm addTask={addTask}/>
+//       {tasks && (
+//         <TaskList
+//           tasks={tasks}
+//           deleteTask={deleteTask}
+//           toggleTask={toggleTask}
+//           enterEditMode={enterEditMode}
+//         />
+//       )}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+import React from 'react'
 import CustomForm from './components/CustomForm'
-import EditForm from './components/EditForm'
-import TaskList from './components/TaskList'
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [previousFocusEl, setPreviousFocusEl] = useState(null);
-  const [editedTask, setEditedTask] = useState(null);
-  const [isEditing, setIsEditing] = useState(false);
-
-  const addTask = (task) => {
-    setTasks(prevState => [...prevState, task])
-  }
-
-  const deleteTask = (id) => {
-    setTasks(prevState => prevState.filter(t => t.id !== id));
-  }
-
-  const toggleTask = (id) => {
-    setTasks(prevState => prevState.map(t => (
-      t.id === id
-        ? { ...t, checked: !t.checked }
-        : t
-    )))
-  }
-
-  const updateTask = (task) => {
-    setTasks(prevState => prevState.map(t => (
-      t.id === task.id
-        ? { ...t, name: task.name }
-        : t
-    )))
-    closeEditMode();
-  }
-
-  const closeEditMode = () => {
-    setIsEditing(false);
-    previousFocusEl.focus();
-  }
-
-  const enterEditMode = (task) => {
-    setEditedTask(task);
-    setIsEditing(true);
-    setPreviousFocusEl(document.activeElement);
-  }
-
+const App = () => {
   return (
-    <div className="container">
-      <header>
-        <h1>My Task List</h1>
-      </header>
-      {
-        isEditing && (
-          <EditForm
-            editedTask={editedTask}
-            updateTask={updateTask}
-            closeEditMode={closeEditMode}
-          />
-        )
-      }
-      <CustomForm addTask={addTask}/>
-      {tasks && (
-        <TaskList
-          tasks={tasks}
-          deleteTask={deleteTask}
-          toggleTask={toggleTask}
-          enterEditMode={enterEditMode}
-        />
-      )}
+    <div>
+      <CustomForm />
     </div>
   )
 }
+
+export default App
--- todolist/src/Components/CustomForm.jsx
+++ todolist/src/Components/CustomForm.jsx
@@ -1,51 +1,103 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-// library imports
-import { PlusIcon } from '@heroicons/react/24/solid'
+// // library imports
+// import { PlusIcon } from '@heroicons/react/24/solid'
 
-const CustomForm = ({ addTask }) => {
-  const [task, setTask] = useState("");
+// const CustomForm = ({ addTask }) => {
+//   const [task, setTask] = useState("");
+
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+//     addTask({
+//       name: task,
+//       checked: false,
+//       id: Date.now()
+//     })
+//     setTask("")
+//   }
+
+//   return (
+//     <form
+//       className="todo"
+//       onSubmit={handleFormSubmit}
+//       >
+//       <div className="wrapper">
+//         <input
+//           type="text"
+//           id="task"
+//           className="input"
+//           value={task}
+//           onInput={(e) => setTask(e.target.value)}
+//           required
+//           autoFocus
+//           maxLength={60}
+//           placeholder="Enter Task"
+//         />
+//         <label
+//           htmlFor="task"
+//           className="label"
+//         >Enter Task</label>
+//       </div>
+//       <button
+//         className="btn"
+//         aria-label="Add Task"
+//         type="submit"
+//         >
+//         <PlusIcon />
+//       </button>
+//     </form>
+//   )
+// }
+// export default CustomForm
+
+
+import React from 'react'
+
+const CustomForm = () => {
+
 
   const handleFormSubmit = (e) => {
-    e.preventDefault();
-    addTask({
-      name: task,
-      checked: false,
-      id: Date.now()
-    })
-    setTask("")
-  }
+        e.preventDefault();
+       addTask({
+           name: task,
+         checked: false,
+          id: Date.now()
+        })
 
   return (
-    <form
-      className="todo"
+    <div>
+      <form 
+      className='todo' 
       onSubmit={handleFormSubmit}
       >
-      <div className="wrapper">
-        <input
-          type="text"
-          id="task"
-          className="input"
-          value={task}
-          onInput={(e) => setTask(e.target.value)}
-          required
-          autoFocus
-          maxLength={60}
-          placeholder="Enter Task"
-        />
+      <div className='wrapper'>
+        <input type="text"
+         id="task" 
+         className='input'
+         value={task} 
+         onInput={(e) => setTask(e.target.value)}
+         required
+         autoFocus
+         maxLength={60}
+         placeholder="Enter Task"
+         />
         <label
-          htmlFor="task"
-          className="label"
-        >Enter Task</label>
-      </div>
-      <button
-        className="btn"
-        aria-label="Add Task"
-        type="submit"
+        htmlFor="task"
+         lassName="label"
         >
+          Enter Task
+       </label>
+        </div> 
+        <button
+       className="btn"
+       aria-label="Add Task"
+       type="submit"
+       >
         <PlusIcon />
-      </button>
-    </form>
+       </button>
+      </form>
+    </div>
   )
+}
 }
 export default CustomForm

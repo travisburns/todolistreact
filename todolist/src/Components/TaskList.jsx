9762e9ccf3b17d90c18824ea3669ed2e9b2bdd23
Travis <travisjb0@gmail.com>
2023-08-03 18:00:03 -0700

    Practiced part 3 for the taskList. Did need to reference notes, however, had remembered very quickly

--- todolist/src/Components/TaskList.jsx
+++ todolist/src/Components/TaskList.jsx
@@ -83,22 +83,23 @@
 
 
 // component import
-import TaskItem from './TaskItem';
-
-// styles
-import styles from './TaskList.module.css';
+import React from 'react'
+import TaskItem from './TaskiTem'
 
 const TaskList = ({tasks}) => {
   return (
-    <ul className={styles.tasks}>
-      {tasks.sort((a, b) => b.id - a.id).map(task => (
-        <TaskItem
-          key={task.id}
-          task={task}
-        />
-      ))
-      }
-    </ul>
+
+      <ul className='styles.tasks'>
+        {tasks.sort((a, b) => b.id - a.id).map(task => (
+        <TaskItem 
+            key={task.id}
+            task={task}
+          />
+          ))
+        }
+      </ul>
+
   )
 }
+
 export default TaskList
